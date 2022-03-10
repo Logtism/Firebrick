eb4d32d24acec9f8fb1b548c4a6496703ab55e5d
Logtism <78323275+Logtism@users.noreply.github.com>
2022-03-11 09:14:22 +1300

    updated and added tests
    - Updated `get_reverse_url` to now work with args
    - Added `GetViewOr404Test`
    - Added `BasicGETOr404ViewTest`

--- firebrick/tests/test.py
+++ firebrick/tests/test.py
@@ -1,17 +1,22 @@
 from django.urls import resolve, reverse
 from django.test import Client
+from django.management import call_command
 
 
-def get_reverse_url(name):
-    if '/' not in name:
-        return reverse(name)
+def get_reverse_url(instance):
+    if '/' not in instance.name:
+        if 'args'in dir(instance):
+           return reverse(instance.name, args=instance.args)
+        elif 'arg_success' in dir(instance):
+            return reverse(instance.name, args=instance.arg_success)
+        return reverse(instance.name)
     else:
-        return name
+        return instance.name
 
 
 class ResolveUrlTest:
-    def test_url_is_resolved(self):   
-        url = get_reverse_url(self.name)
+    def test_url_is_resolved(self):
+        url = get_reverse_url(self)
         
         if '__func__' in dir(self.view):
             self.assertEquals(resolve(url).func, self.view.__func__)
@@ -23,9 +28,38 @@ class GetViewTest:
     def test_GET(self):
         client = Client()
         
-        url = get_reverse_url(self.name)
+        url = get_reverse_url(self)
         
         response = client.get(url)
         
         self.assertEquals(response.status_code, self.status)
         self.assertTemplateUsed(response, self.template)
+
+
+class GetViewOr404Test:
+    def test_GET_success(self):
+        client = Client()
+        
+        call_command('loaddata', *self.fixtures, verbosity=0)
+        
+        url = reverse(self.name, args=self.arg_success)
+        
+        response = client.get(url)
+        
+        self.assertEquals(response.status_code, self.status_success)
+        self.assertTemplateUsed(response, self.template_success)
+        
+    def test_GET_404(self):
+        client = Client()
+        
+        call_command('loaddata', *self.fixtures, verbosity=0)
+        
+        url = reverse(self.name, args=self.arg_fail)
+        
+        response = client.get(url)
+        
+        self.assertEquals(response.status_code, self.status_fail)
+        self.assertTemplateNotUsed(response, self.template_success)
+        
+        if 'template_fail' in dir(self):
+            self.assertTemplateUsed(response, self.template_fail)
