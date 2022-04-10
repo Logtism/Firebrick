8d2ad41719e29b3b78ce7b4722b610b11451d6da
Logtism <78323275+Logtism@users.noreply.github.com>
2022-04-10 13:30:38 +1200

    Fixed tests imports

--- firebrick/tests/test.py
+++ firebrick/tests/test.py
@@ -2,7 +2,7 @@ from django.test import TestCase, Client
 from django.urls import reverse, resolve
 from django.core.management import call_command
 from django.conf import settings
-from django.contrib.auth.models import User
+
 
 def get_reverse_url(instance, args=None):
     if '/' not in instance.name:
@@ -103,6 +103,7 @@ class GETLoginRequiredTest:
 
     def test_GET_logged_in(self):
         # Creating a user
+        from django.contrib.auth.models import User
         User.objects.create_user(username='testuser1', password='password1')
 
         client = Client()
